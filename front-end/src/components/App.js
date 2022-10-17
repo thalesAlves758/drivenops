42bb1e5487995f7e36bcdaf4de20766c43204142
Thales Alves <thalesalves758@gmail.com>
2022-10-17 14:37:48 -0300

    feat: use react-router-dom and new external components as pages

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,25 +1,16 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Students from './Students';
 
 function App() {
-  const [student, setStudent] = useState(null);
-
-  useEffect(async () => {
-    const API_URL = process.env.REACT_APP_BASE_URL;
-    try {
-      const response = await axios.get(`${API_URL}/students/random`);
-      const student = response.data;
-      if (!student) {
-        alert('Xablau! Não há estudantes cadastrados para o sorteio!');
-      } else {
-        setStudent(student);
-      }
-    } catch (error) {
-      alert('Não foi possível realizar o sorteio!');
-    }
-  }, []);
-
-  return student ? <h1>{student.name}</h1> : 'Carregando...';
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/students' element={<Students />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
